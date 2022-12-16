a22ea9c14637b5a75a3310a0bd11a180f5601c6a
visurm76 <visurm76@gmail.com>
2022-12-16 15:06:39 +0300

    Зарегистрировали модели в admin.py

--- WebBooks/catalog/admin.py
+++ WebBooks/catalog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Autor, Book, Genre, Language, Status, Bookinstance
 
-# Register your models here.
+
+admin.site.register(Autor)
+admin.site.register(Book)
+admin.site.register(Genre)
+admin.site.register(Language)
+admin.site.register(Status)
+admin.site.register(Bookinstance)
+
